1255a99c6898d8b3fe36398a52ed49b669a5e46a
abdelmoaty299 <154735333+abdelmoaty299@users.noreply.github.com>
2023-12-24 13:53:51 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+document.getElementById('b1').addEventListener('click',function(){
+window.location.href=('login.html')})
+document.getElementById('send').addEventListener('click',function(){
+window.location.href=('send.html')   
+})
+document.getElementById('follow').addEventListener('click',function(){
+window.location.href=('follow.html')
+})
